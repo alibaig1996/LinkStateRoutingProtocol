85029c87c74260b518e8afab47cb38c3a5ff60c5
Ali <ali.baig1996@gmail.com>
2016-12-07 21:24:57 +0500

    Initial code. Reading text file and parsing information in it

---
+++ test.py
@@ -0,0 +1,38 @@
+import threading
+import socket
+import sys
+import os.path
+
+def ReceiveInformation(name):
+
+
+def Main():
+	
+	if len(sys.argv) < 4:
+		print "Not enough arguments for script to execute"
+		sys.exit()
+
+	routerId = sys.argv[1]
+	portNo = sys.argv[2]
+	configFile = "routers\\" + sys.argv[3]
+
+	if not os.path.isfile(configFile):
+		print "Config file does not exist"
+		sys.exit()
+
+	neighbors = []
+	with open(configFile, 'r') as f:
+		noOfNeighbors = f.readline()[0]
+
+		for line in f:
+			neighbors.append(tuple(line.split()))
+
+	print neighbors
+
+	s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+	s.bind(('', portNo));
+	
+	print "Socket bound to port " + portNo
+
+if __name__ == '__main__':
+	Main()
