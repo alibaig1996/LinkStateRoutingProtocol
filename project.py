85c727441415d0410910ebe17e5b3b7f5c393b07
Ali <ali.baig1996@gmail.com>
2016-12-21 19:12:07 +0500

    Changes in graph data structure

--- project.py
+++ project.py
@@ -3,17 +3,15 @@ import socket
 import sys
 import os.path
 import time
+import json
 
 neighbors = []
 graph = {}
-mutex = threading.Lock()
 s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 
 def sendLSA():
 	while True:
 		time.sleep(5)
-		print "Sending packets"
-		print s
 		routerId = sys.argv[1]
 		parentString = sys.argv[1]
 
@@ -22,28 +20,24 @@ def sendLSA():
 
 			parentString += receiverString
 
-		for i in range(0, len(neighbors)):
-			prtNo = graph[routerId][i][2]
+		for i in neighbors:
+			prtNo = graph[routerId][i[0]][1]
 			s.sendto(parentString, ('localhost', prtNo))
 
 def receiveLSA():
 	while True:
-		print "Waiting..."
 		msg, addr = s.recvfrom(1024)
-		print "Received " + str(len(msg)) + "bytes of data from " + str(addr)
 		msgList = msg.splitlines()
-
-		print msg
 
 		sendingRouter = msgList[0]
 
 		# Update graph
 
 		if sendingRouter not in graph.keys():
-			graph[sendingRouter] = []
+			graph[sendingRouter] = {}
 			for i in range(1, len(msgList)):
 				router, cost, prtNo = msgList[i].split()
-				graph[sendingRouter].append((router, float(cost), int(prtNo)))
+				graph[sendingRouter][router] =  (float(cost), int(prtNo))
 
 
 		# Broadcast LSU packet to neighbours
@@ -56,10 +50,10 @@ def receiveLSA():
 			else:
 				s.sendto(msg, ('localhost', int(x[2])))
 
+		print json.dumps(graph, sort_keys=True, indent=4, separators=(',', ': '))
+
 
 def dijkstrasAlgo():
-	mutex.acquire()
-	print "Hi 3"
 	mutex.release()
 
 def Main():
@@ -83,18 +77,14 @@ def Main():
 			router, cost, prtNo = line.split()
 			neighbors.append((router, cost, prtNo))
 
-	print neighbors
-
 	s.bind(('localhost', int(portNo)));
-	
-	print "Socket bound to port " + portNo
 
 	# Initialise graph
 
-	graph[routerId] = []
+	graph[routerId] = {}
 
 	for x in neighbors:
-		graph[routerId].append((x[0], float(x[1]), int(x[2])))
+		graph[routerId][x[0]] =  (float(x[1]), int(x[2]))
 
 	print graph
 
