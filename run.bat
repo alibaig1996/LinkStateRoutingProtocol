f59728d954887289b97f9cab2971ff348cb5ccd7
Ali <ali.baig1996@gmail.com>
2016-12-30 11:02:22 +0500

    Deleted gitignore

---
+++ run.bat
@@ -0,0 +1,6 @@
+start python project.py A 5000 ConfigA.txt
+start python project.py B 5001 ConfigB.txt
+start python project.py C 5002 ConfigC.txt
+start python project.py D 5003 ConfigD.txt
+start python project.py E 5004 ConfigE.txt
+start python project.py F 5005 ConfigF.txt
